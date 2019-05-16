4d36ee68accef9710eb5d945a708f126d20cd80e
github username <github email address>
2019-05-16 10:47:17 -0400

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,54 @@
+class Song
+
+  attr_accessor :name, :artist, :genre
+
+  @@count = 0
+  @@genres = []
+  @@artists = []
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    @@genres << self.genre
+    @@artists << self.artist
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.genres
+    @@genres.uniq
+  end
+
+  def self.artists
+    @@artists.uniq
+  end
+
+  def self.genre_count
+    gen_count = {}
+    @@genres.each do |genre|
+      if gen_count[genre]
+        gen_count[genre] += 1
+      else
+        gen_count[genre] = 1
+      end
+    end
+    gen_count
+  end
+
+  def self.artist_count
+    art_count = {}
+    @@artists.each do |artist|
+      if art_count[artist]
+        art_count[artist] += 1
+      else
+        art_count[artist] = 1
+      end
+    end
+    art_count
+  end
+
+end
